71f599eda964cb30b58426184a3ff5c3ff8f40d3
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-13 09:21:31 +0900

    課題２　完成

--- Loco.Kadai2/AppDelegate.swift
+++ Loco.Kadai2/AppDelegate.swift
@@ -1,9 +1,3 @@
-//
-//  AppDelegate.swift
-//  Loco.Kadai2
-//
-//  Created by 日高隼人 on 2021/06/12.
-//
 
 import UIKit
 
--- Loco.Kadai2/ViewController.swift
+++ Loco.Kadai2/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  Loco.Kadai2
-//
-//  Created by 日高隼人 on 2021/06/12.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak private var firstTextField: UITextField!
+    @IBOutlet weak private var secondTextField: UITextField!
+    @IBOutlet weak private var segmentedControlFourArithmeticOperations: UISegmentedControl!
+    @IBOutlet weak private var calculateDisplayLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    private func toDisplayCalculation() {
+        let selectSegmentIndex: Int = segmentedControlFourArithmeticOperations.selectedSegmentIndex
+        guard let firstFieldNum = Double(firstTextField.text!), let secondFieldNum = Double(secondTextField.text!) else {
+            calculateDisplayLabel.text! = "テキストフィールドが空欄です。"
+            return }
 
+        switch selectSegmentIndex {
+        case 0:
+            calculateDisplayLabel.text! = String(firstFieldNum + secondFieldNum)
+        case 1:
+            calculateDisplayLabel.text! = String(firstFieldNum - secondFieldNum)
+        case 2:
+            calculateDisplayLabel.text! = String(firstFieldNum * secondFieldNum)
+        case 3:
+            if firstFieldNum == 0 || secondFieldNum == 0 {
+                calculateDisplayLabel.text! = "割り算には、０以外を記入。"
+            } else {
+                calculateDisplayLabel.text! = String(firstFieldNum / secondFieldNum)
+            }
+        default:
+            break
+        }
+    }
+    @IBAction private func calculateButton(_ sender: Any) {
+        toDisplayCalculation()
+    }
 }
-
