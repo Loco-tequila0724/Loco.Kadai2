3a7e357344fe157acbb0b010c5ba3811dfb52471
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-14 11:45:28 +0900

    フィードバック済　完成

--- Loco.Kadai2/ViewController.swift
+++ Loco.Kadai2/ViewController.swift
@@ -1,18 +1,23 @@
 import UIKit
 
+// メソッド名には、動詞を初めに書く
+// selectedSegmentIndexはいったん定数に入れず、switchに直接書いても大丈夫
+// 0 ÷ 10 などは許容して（答えは10）、10 ÷ 0 などを禁止したいので（ゼロ除算。数学において、これを矛盾なく定義することはできない）、secondFieldNumのみをチェックします
+// ゼロ除算 - Wikipedia
+
 class ViewController: UIViewController {
     @IBOutlet weak private var firstTextField: UITextField!
     @IBOutlet weak private var secondTextField: UITextField!
     @IBOutlet weak private var segmentedControlFourArithmeticOperations: UISegmentedControl!
     @IBOutlet weak private var calculateDisplayLabel: UILabel!
 
-    private func toDisplayCalculation() {
-        let selectSegmentIndex: Int = segmentedControlFourArithmeticOperations.selectedSegmentIndex
+    private func calculateAndDisplayResult() {
         guard let firstFieldNum = Double(firstTextField.text!), let secondFieldNum = Double(secondTextField.text!) else {
             calculateDisplayLabel.text! = "テキストフィールドが空欄です。"
-            return }
+            return
+        }
 
-        switch selectSegmentIndex {
+        switch segmentedControlFourArithmeticOperations.selectedSegmentIndex {
         case 0:
             calculateDisplayLabel.text! = String(firstFieldNum + secondFieldNum)
         case 1:
@@ -20,7 +25,7 @@ class ViewController: UIViewController {
         case 2:
             calculateDisplayLabel.text! = String(firstFieldNum * secondFieldNum)
         case 3:
-            if firstFieldNum == 0 || secondFieldNum == 0 {
+            if secondFieldNum == 0 {
                 calculateDisplayLabel.text! = "割り算には、０以外を記入。"
             } else {
                 calculateDisplayLabel.text! = String(firstFieldNum / secondFieldNum)
@@ -30,6 +35,6 @@ class ViewController: UIViewController {
         }
     }
     @IBAction private func calculateButton(_ sender: Any) {
-        toDisplayCalculation()
+        calculateAndDisplayResult()
     }
 }
